c499a98b953d1cf81afc514cc23deacd4401c6d2
Ronald Cuello <ronald.cuello@softwareone.com>
2025-03-20 14:28:47 -0500

    Nuevo metodo para realizar deposito

--- src/main/java/edu/uni/comfenalco/tecnobanco/Aplicacion.java
+++ src/main/java/edu/uni/comfenalco/tecnobanco/Aplicacion.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
 
 public class Aplicacion {
     static double saldoUsuario = 20000;
+    static String usuarioAutenticadoNombre = "usuario";
+    static String usuarioAutenticadoClave = "1234";
+    static String usuarioAutenticadoIdentificacion = "USR007";
 
     public static void main(String[] args) {
 
@@ -25,7 +28,7 @@ public class Aplicacion {
         char[] passwordArray = console.readPassword("Ingrese su contraseña: ");
         String usuarioClave = new String(passwordArray);
 
-        if (usuarioNombre.equals("usuario") && usuarioClave.equals("1234")) {
+        if (usuarioNombre.equals(usuarioAutenticadoNombre) && usuarioClave.equals(usuarioAutenticadoClave)) {
             System.out.println(" ===============================");
             System.out.println("|  OPERACION COMPLETADA         |");
             System.out.println("|  Ha iniciado sesión con éxito |");
@@ -54,7 +57,7 @@ public class Aplicacion {
 
                 switch (opcion) {
                     case 1:
-                        System.out.println("TODO: Realizar deposito");
+                        realizarDeposito(scanner);
                         break;
                     case 2:
                         System.out.println("TODO: Realizar transferencia");
@@ -63,7 +66,7 @@ public class Aplicacion {
                         mostrarSaldo();
                         break;
                     case 4:
-                        System.out.println("TODO: Mostrar informacion del usuario");
+                        mostrarInformacionUsuario();
                         break;
                     case 5:
                         System.out.println("Saliendo del sistema...");
@@ -79,8 +82,33 @@ public class Aplicacion {
         }
     }
 
+    public static void realizarDeposito(Scanner scanner) {
+        System.out.print("Ingrese el monto a depositar: ");
+        double monto = scanner.nextDouble();
+
+        if (monto > 0) {
+            saldoUsuario += monto;
+            String saldoFormateado = formatearMoneda(saldoUsuario);
+            System.out.println("Depósito realizado con éxito. Su nuevo saldo es: " + saldoFormateado);
+        } else {
+            System.out.println("El monto debe ser mayor que 0.");
+        }
+    }
+
     public static void mostrarSaldo() {
         System.out.println("Su saldo actual es: " + formatearMoneda(saldoUsuario));
+    }
+
+    public static void mostrarInformacionUsuario() {
+        String saldoFormateado = formatearMoneda(saldoUsuario);
+
+        System.out.println("+-------------------------------+");
+        System.out.println("| ** Información del Usuario ** |");
+        System.out.println("+-------------------------------+");
+        System.out.println("* Nombre de usuario: " + usuarioAutenticadoNombre);
+        System.out.println("* ID de usuario: " + usuarioAutenticadoIdentificacion);
+        System.out.println("* Saldo actual: " + saldoFormateado);
+        System.out.println("+-------------------------------+");
     }
 
     // Método para formatear números como moneda
