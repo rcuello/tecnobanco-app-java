ab5628cee5d840583b2e08ee203c30d6ee80641b
Ronald Cuello <ronald.cuello@softwareone.com>
2025-03-20 14:03:36 -0500

    Menu principal con validacion de opciones

--- src/main/java/edu/uni/comfenalco/tecnobanco/Aplicacion.java
+++ src/main/java/edu/uni/comfenalco/tecnobanco/Aplicacion.java
@@ -28,6 +28,51 @@ public class Aplicacion {
             System.out.println("|  OPERACION COMPLETADA         |");
             System.out.println("|  Ha iniciado sesión con éxito |");
             System.out.println(" ===============================");
+
+            boolean salirDelPrograma = false;
+            
+            while (!salirDelPrograma) {
+
+                System.out.println("+-------------------------------+");
+                System.out.println("|        ** Menú Principal **   |");
+                System.out.println("+-------------------------------+");
+                System.out.println("| 1. Depositar                  |");
+                System.out.println("| 2. Transferir                 |");
+                System.out.println("| 3. Ver saldo                  |");
+                System.out.println("| 4. Información del usuario    |");
+                System.out.println("| 5. Salir                      |");
+                System.out.println("+-------------------------------+");
+
+                System.out.print("Seleccione una opción: ");
+
+                int opcion = scanner.nextInt();
+
+                // Hacer un salto de Linea en la pantalla
+                scanner.nextLine();
+
+                switch (opcion) {
+                    case 1:                        
+                        System.out.println("TODO: Realizar deposito");
+                        break;
+                    case 2:                        
+                        System.out.println("TODO: Realizar transferencia");
+                        break;
+                    case 3:                        
+                        System.out.println("TODO: Mostrar saldo");
+                        break;
+                    case 4:                        
+                        System.out.println("TODO: Mostrar informacion del usuario");
+                        break;
+                    case 5:
+                        System.out.println("Saliendo del sistema...");
+                        salirDelPrograma = true;
+                        break;
+                    default:
+                        System.out.println("Opción inválida. Por favor, seleccione una opción del 1 al 5.");
+                }
+            }
+
+
         } else {
             System.out.println("Credenciales invalidas!");
         }
