af6532ea5c60fee44b855df5a611b1d04f5b7f21
Ronald Cuello <ronald.cuello@softwareone.com>
2025-03-20 13:38:05 -0500

    Mensaje bienvenida TecnoBanco

--- src/main/java/edu/uni/comfenalco/tecnobanco/Aplicacion.java
+++ src/main/java/edu/uni/comfenalco/tecnobanco/Aplicacion.java
@@ -13,74 +13,11 @@ public class Aplicacion {
 
     public static void main(String[] args){
 
-        Scanner scanner = new Scanner(System.in);
-        Console console = System.console();
-
         System.out.println("+-------------------------------+");
         System.out.println("|       ** Bienvenido a         |");
         System.out.println("|        TecnoBanco **          |");
         System.out.println("+-------------------------------+");
 
-        System.out.println("Ingrese el nombre de usuario:");
-
-        String usuarioNombre = scanner.nextLine();
-
-        char[] passwordArray = console.readPassword("Ingrese su contraseña: ");
-        String usuarioClave = new String(passwordArray);
-
-        if (usuarioNombre.equals(usuarioAutenticadoNombre) && usuarioClave.equals(usuarioAutenticadoClave)) {
-
-            System.out.println("================================");
-            System.out.println("|  OPERACION COMPLETADA         |");
-            System.out.println("|  Ha iniciado sesión con éxito |");
-            System.out.println("================================");
-
-            boolean salirDelPrograma = false;
-
-            while (!salirDelPrograma) {
-
-                System.out.println("+-------------------------------+");
-                System.out.println("|        ** Menú Principal **   |");
-                System.out.println("+-------------------------------+");
-                System.out.println("| 1. Depositar                  |");
-                System.out.println("| 2. Transferir                 |");
-                System.out.println("| 3. Ver saldo                  |");
-                System.out.println("| 4. Información del usuario    |");
-                System.out.println("| 5. Salir                      |");
-                System.out.println("+-------------------------------+");
-
-                System.out.print("Seleccione una opción: ");
-
-                int opcion = scanner.nextInt();
-
-                // Hacer un salto de Linea en la pantalla
-                scanner.nextLine();
-
-                switch (opcion) {
-                    case 1:
-                        realizarDeposito(scanner);
-                        break;
-                    case 2:
-                        realizarTransferencia(scanner);
-                        break;
-                    case 3:
-                        mostrarSaldo();
-                        break;
-                    case 4:
-                        mostrarInformacionUsuario();
-                        break;
-                    case 5:
-                        System.out.println("Saliendo del sistema...");
-                        salirDelPrograma = true;
-                        break;
-                    default:
-                        System.out.println("Opción inválida. Por favor, seleccione una opción del 1 al 5.");
-                }
-            }
-
-        } else {
-            System.out.println("Credenciales invalidas!");
-        }
 
     }
 
